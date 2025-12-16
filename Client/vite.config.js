595900a52606cc60ad3726c96d1dc430aa851924
Satyam Pandey <ofcsatyam007@gmail.com>
2025-12-16 14:44:32 +0530

    fixing some bugs

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
-    outDir: "dist"
+    outDir: "dist",
+    assetsDir: "assets"
   }
 });
